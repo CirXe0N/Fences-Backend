03efb0d116985ecb9b98a18935cbbdbc254db811
CirXe0N <dw1ls@outlook.com>
2017-06-11 21:47:24 +0200

    Add functionality to delete room when user disconnects and Send messages on connect/disconnect events

--- api/consumers.py
+++ api/consumers.py
@@ -9,27 +9,62 @@ from api.models import GameRoom
 def ws_connect(message):
     message.reply_channel.send({"accept": True})
     room_id = message.content['path'].strip("/")
-    room = GameRoom.objects.get(room_id=room_id)
-    # TODO Refactor this
-    if room.player_two:
-        room.player_two_reply_channel = message.reply_channel
-    else:
-        room.player_one_reply_channel = message.reply_channel
-    room.save()
-    message.channel_session['room_id'] = room_id
-    Group("chat-%s" % room_id).add(message.reply_channel)
+
+    try:
+        room = GameRoom.objects.get(room_id=room_id)
+
+        message.channel_session['room_id'] = room_id
+        Group("chat-%s" % room_id)
+        group = Group("chat-%s" % room_id)
+        group.add(message.reply_channel)
+
+        content = {
+            'type': 'CONNECT',
+            'sent_at': datetime.now().isoformat()
+        }
+
+        if room.player_two:
+            room.player_two_reply_channel = message.reply_channel
+            content['user_id'] = '%s' % room.player_one
+            group.send({"text": json.dumps(content)})
+
+            content['user_id'] = '%s' % room.player_two
+            group.send({"text": json.dumps(content)})
+        else:
+            room.player_one_reply_channel = message.reply_channel
+        room.save()
+
+    except GameRoom.DoesNotExist:
+        pass
 
 
 @channel_session
 def ws_disconnect(message):
     room_id = message.channel_session['room_id']
+    group = Group("chat-%s" % room_id)
+
+    content = {
+        'type': 'DISCONNECT',
+        'sent_at': datetime.now().isoformat()
+    }
+
+    group.send({"text": json.dumps(content)})
+
+    rooms = GameRoom.objects.filter(room_id=room_id)
+    for room in rooms:
+        room.delete()
+
     Group("chat-%s" % room_id).discard(message.reply_channel)
 
 
 @channel_session
 def ws_message(message):
     content = json.loads(message.content['text'])
-    content['sent_at'] = datetime.now().isoformat()
+
+    if content.get('type') == 'MESSAGE':
+        content['sent_at'] = datetime.now().isoformat()
+
     room_id = message.channel_session['room_id']
-    Group("chat-%s" % room_id).send({"text": json.dumps(content)})
+    group = Group("chat-%s" % room_id)
+    group.send({"text": json.dumps(content)})
 
